20e570b7ff2b20e5c1b8a45104094406aec1e72a
usac201612146 <3263989101014@ingenieria.usac.edu.gt>
2020-06-22 14:07:54 -0600

    parcial 2

---
+++ cliente1/brokerData.py
@@ -0,0 +1,87 @@
+#Parametros de conexion
+#"157.245.82.242"
+MQTT_HOST = "167.71.243.238"
+MQTT_PORT = 1883
+
+#MQTT_USER = "pr24"
+#MQTT_PASS = "Patito!#2020"
+MQTT_USER = "proyectos"
+MQTT_PASS = "proyectos980"
+arch = 'Archivos.tex'
+
+#Credenciales
+#Se acostumbra solicitar al usuario que ingrese su user/pass
+#no es buena practica dejar escritas en el codigo las credenciales
+def read(a):
+    LISTADO = a
+    datos = []
+    archivo = open(LISTADO, 'r')
+    registro = archivo.readlines()
+    for i in range(len(registro)):
+        datos.append(registro[i].replace('\n', '') ) 
+    archivo.close()
+    return datos
+datos = read(arch)
+usuario = datos[1]
+sala1= datos[3]
+sala2= datos[4]
+
+
+#comaistrondos
+FTR = b'\x03'       #audio
+ALIVE = b'\x04'     #alive
+ACK = b'\x05'
+#OK = b'\x06'
+#NO = b'\x07'
+SEPARADOR = b'$'
+#destinos o topics de subscripcion
+qos = 0
+user1 = 201612625   #titus
+user2 = 201612146   #sebas
+user1_t = b'201612625'   #titus
+user_t =datos[1].encode()  #sebas
+GRUPO = 24
+test = ('test',qos)
+#mensaje
+topic_user2 = 'usuarios/'+str(GRUPO)+'/'+str(user1)
+#alive
+topicComandos_alive = 'comandos/'+str(GRUPO)
+tramaALIV = ALIVE+user1_t  
+#salas
+topic_sala1 = 'salas/'+str(GRUPO)+'/'+sala1
+topic_sala1 = 'salas/'+str(GRUPO)+'/'+sala2
+#subs
+SUBS_comandos2 = 'comandos/'+str(GRUPO)+'/'+usuario
+SUBS_comandos = ('comandos/'+str(GRUPO)+'/'+usuario, qos)
+SUBS_usuario = ('usuarios/'+str(GRUPO)+'/'+usuario, qos)
+SUBS_sala1 = ('salas/'+ str(GRUPO)+'/'+sala1, qos)
+SUBS_sala2 = ('salas/'+ str(GRUPO)+'/'+sala2, qos)
+#tramas de comandos
+trama_ACK = ACK+user_t
+
+
+#class comandos (object):
+class comandos(object):
+    def __init__(self, data = []):
+        self.data = list(data)
+
+    #La "longitud" del objeto, es en realidad representado
+    #por la cantidad de datos de su lista principal
+    def __len__(self):
+        return len(self.data)
+    def __str__(self):
+        return str(self.data)
+
+    def comp (self,i,e):
+        if  len(self.data) != i:
+            e += 1
+            i+=1
+            if e <= 3:
+                ALIVE_PERIOD = 0.1
+                return ALIVE_PERIOD,i,e
+        if len(self.data) ==i:
+            ALIVE_PERIOD = 2
+            return ALIVE_PERIOD,i,e
+    #Representacion cuando se invoca el objeto sin casting a STRING.
+    def __repr__(self):
+        return self.__str__()
---
+++ cliente1/mqtt.py
@@ -0,0 +1,131 @@
+import paho.mqtt.client as paho
+import logging
+import time
+import random
+import threading #Concurrencia con hilos
+import sys 
+import os 
+#from mqttTestSubscription import *
+from brokerData import* #Informacion de la conexion
+
+'''
+Ejemplo de cliente MQTT: gateway de red de sensores
+'''
+LOG_FILENAME = 'mqtt.log'
+os.remove("mqtt.log")
+
+#Configuracion inicial de logging
+logging.basicConfig(
+    level = logging.INFO, 
+    format = '[%(levelname)s] (%(processName)-10s) %(message)s'
+    )
+      
+#Tiempo de espera entre lectura y envio de dato de sensores a broker (en segundos)
+
+
+#mensaje de alive cada 2 segundos
+'''
+def alive(ALIVE_PERIOD):
+  # se envia un menaje cada 2 segundos indicando que sigue conectado
+    client.publish(SUBS_comandos2 , tramaALIV, qos = 0,retain = False)
+    time.sleep(ALIVE_PERIOD)
+'''
+      
+#Handler en caso suceda la conexion con el broker MQTT
+def on_connect(client, userdata, flags, rc): 
+    client.subscribe([SUBS_comandos,SUBS_usuario,SUBS_sala1,SUBS_sala2, ])
+
+#Handler en caso se publique satisfactoriamente en el broker MQTT
+
+def on_publish(client, userdata, mid): 
+    publishText = "Publicacion satisfactoria"
+    logging.debug( str(publishText) )
+
+
+def on_message(client, userdata, msg):
+    #Se muestra en pantalla informacion que ha llegado
+    logging.debug("Ha llegado el mensaje al topic: " + str(msg.topic))
+    logging.debug("El contenido del mensaje es: " + str(msg.payload))
+    #Y se almacena en el log
+    filename= 'mqtt.log'
+    archivo = open(filename,'a') #Abrir para SOBREESCRIBIR el archivo existente
+    archivo.write(str(msg.topic) +  '-> ' + str(msg.payload) +'\n')
+    archivo.close()
+
+
+logging.info("Cliente MQTT con paho-mqtt") #Mensaje en consola
+
+'''
+Config. inicial del cliente MQTT
+'''
+client = paho.Client(clean_session=True) #Nueva instancia de cliente
+client.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
+client.on_publish = on_publish #Se configura la funcion "Handler" que se activa al publicar algo
+client.on_message = on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
+client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
+client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
+
+
+client.loop_start()
+
+
+#hilo alive
+
+
+t1 = threading.Thread (name = 'verificacion',
+                            target = audio,
+                            daemon = True
+                            )
+
+t1.start()
+#Loop principal: leer los datos de los sensores y enviarlos al broker en los topics adecuados cada cierto tiempo
+try:
+    while True:
+        print('1. Enviar texto')
+        print('2. Enviar mensaje de voz')
+        print('3. Salir')
+        x = input('-:')
+    #vemos que hacción hacer y a quien enviarlo
+        #enviar mensaje 
+        if x == '1' :  #usurio 1
+            print('a. Enviar a usuario')
+            print('b. Enviar a sala')
+            t = input('-:')
+            if t == 'a':
+                print('1. 201612625')
+                print('2. ---')
+                t = input('-:')
+                if x == '1' :
+                    t = input('escriba el texto:  ')
+                    trama = user_t + SEPARADOR + t.encode() #codifica el mensaje 
+                    #enviamos el mensaje
+                    client.publish(topic_user2, trama, qos = 0,retain = False)
+                else:
+                    t = 0
+                    x= 0
+            if t == 'b':
+                print('1. 24S15')
+                print('2. 24S11')
+                t = input('-:')
+                if x == '1' :
+                    t = input('escriba el texto:  ')
+                    trama = user_t + SEPARADOR + t.encode() #codifica el mensaje 
+                    #enviamos el mensaje
+                    client.publish(topic_user2, trama, qos = 0,retain = False)
+        if x == '3':
+            logging.warning("Desconectando del broker MQTT...")
+            client.loop_stop()
+            if t1.isAlive():
+                t1._stop()
+            break
+        else:
+            print('numero incorrecto')
+except KeyboardInterrupt:
+    logging.warning("Desconectando del broker MQTT...")
+    client.loop_stop()
+    if t1.isAlive():
+        t1._stop()
+
+finally:
+    client.disconnect()
+    logging.info("Se ha desconectado del broker. Saliendo...")
